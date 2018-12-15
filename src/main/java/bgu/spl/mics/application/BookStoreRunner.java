925e325e4e9d9a0369ebb343d721bd89716cac03
ronasis <ronasi@post.bgu.ac.il>
2018-12-15 21:43:01 +0200

    add Package accessoress and change messagebuslmpl

--- src/main/java/bgu/spl/mics/application/BookStoreRunner.java
+++ src/main/java/bgu/spl/mics/application/BookStoreRunner.java
@@ -3,14 +3,8 @@ package bgu.spl.mics.application;
 import bgu.spl.mics.application.accessoires.ReadJson;
 import bgu.spl.mics.application.accessoires.RunnerTherds;
 import bgu.spl.mics.application.accessoires.WriteObjectToFile;
-import bgu.spl.mics.application.passiveObjects.*;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.util.HashMap;
-import java.util.List;
+import bgu.spl.mics.application.passiveObjects.Inventory;
+import bgu.spl.mics.application.passiveObjects.MoneyRegister;
 
 /** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
@@ -28,29 +22,8 @@ public class BookStoreRunner {
         RunnerTherds runnerTherds=new RunnerTherds(readJson.getThreads());
         runnerTherds.runner();
         printTofiles();
-        try {
-            List<OrderReceipt> or=( List<OrderReceipt>) getReceipts(args[3]);
-            System.out.println("amount of orders "+or.size());
-            HashMap<Integer, Customer> customerHashMap=(HashMap<Integer, Customer>)getReceipts(args[1]);
-            HashMap<String,BookInventoryInfo> booksInventory=(HashMap<String,BookInventoryInfo>)getReceipts(args[2]);
-           MoneyRegister moneyRegister=(MoneyRegister)getReceipts(args[4]);
-            System.out.println("amount of orders "+or.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
-    private static Object getReceipts(String receiptsObj)
-            throws FileNotFoundException, IOException, ClassNotFoundException {
-        FileInputStream fileIn = new FileInputStream(receiptsObj);
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        Object receipts =  in.readObject();
-        in.close();
-        fileIn.close();
-        return receipts;
-    }
     private static void printTofiles(){
         WriteObjectToFile writeObjectToFile=new WriteObjectToFile();
         writeObjectToFile.printToFile(nameFiles[0], readJson.getCustomerHashMap());
